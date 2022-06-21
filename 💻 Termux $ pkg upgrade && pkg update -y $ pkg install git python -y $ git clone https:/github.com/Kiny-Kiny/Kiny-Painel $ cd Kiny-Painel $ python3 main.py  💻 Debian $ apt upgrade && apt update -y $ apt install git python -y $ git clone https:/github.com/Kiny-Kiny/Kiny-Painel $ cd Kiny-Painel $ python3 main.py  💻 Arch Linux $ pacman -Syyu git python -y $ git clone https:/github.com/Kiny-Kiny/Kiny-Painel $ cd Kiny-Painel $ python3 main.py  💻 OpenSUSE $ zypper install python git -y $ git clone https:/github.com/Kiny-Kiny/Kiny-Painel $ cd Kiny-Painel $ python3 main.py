8779e0d4008f06edbc031dc1c1e3bfdc9e976e4d
vtyzi <107928617+vtyzi@users.noreply.github.com>
2022-06-21 08:52:31 -0300

    Create Kiny-Painel $ cd Kiny-Painel $ python3 main.py

---
+++ 💻 Termux $ pkg upgrade && pkg update -y $ pkg install git python -y $ git clone https:/github.com/Kiny-Kiny/Kiny-Painel $ cd Kiny-Painel $ python3 main.py  💻 Debian $ apt upgrade && apt update -y $ apt install git python -y $ git clone https:/github.com/Kiny-Kiny/Kiny-Painel $ cd Kiny-Painel $ python3 main.py  💻 Arch Linux $ pacman -Syyu git python -y $ git clone https:/github.com/Kiny-Kiny/Kiny-Painel $ cd Kiny-Painel $ python3 main.py  💻 OpenSUSE $ zypper install python git -y $ git clone https:/github.com/Kiny-Kiny/Kiny-Painel $ cd Kiny-Painel $ python3 main.py
@@ -0,0 +1,25 @@
+💻 Termux
+$ pkg upgrade && pkg update -y
+$ pkg install git python -y
+$ git clone https://github.com/Kiny-Kiny/Kiny-Painel
+$ cd Kiny-Painel
+$ python3 main.py
+
+💻 Debian
+$ apt upgrade && apt update -y
+$ apt install git python -y
+$ git clone https://github.com/Kiny-Kiny/Kiny-Painel
+$ cd Kiny-Painel
+$ python3 main.py
+
+💻 Arch Linux
+$ pacman -Syyu git python -y
+$ git clone https://github.com/Kiny-Kiny/Kiny-Painel
+$ cd Kiny-Painel
+$ python3 main.py
+
+💻 OpenSUSE
+$ zypper install python git -y
+$ git clone https://github.com/Kiny-Kiny/Kiny-Painel
+$ cd Kiny-Painel
+$ python3 main.py
